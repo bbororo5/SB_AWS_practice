08c40153cd646a22b920e38d39b037471002b119
전선웅 <jeonseon-ung@jeonseon-ung-ui-MacBookAir.local>
2023-04-17 19:11:46 +0900

    Initial commit

---
+++ practice/src/main/groovy/com/SB/AWS/practice/PracticeApplication.groovy
@@ -0,0 +1,13 @@
+package com.SB.AWS.practice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PracticeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PracticeApplication, args)
+	}
+
+}
---
+++ practice/src/test/groovy/com/SB/AWS/practice/PracticeApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.SB.AWS.practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
